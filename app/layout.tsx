528e83f4fd4a8bc65abd42cc45722364f02cdc48
Piotr Bednarski <piotr.maciej.bednarski@gmail.com>
2025-02-09 08:15:06 +0100

    format layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,33 @@
-import { RootProvider } from 'fumadocs-ui/provider';
-import 'fumadocs-ui/style.css';
-import 'fumadocs-ui/css/dusk.css';
-import 'fumadocs-twoslash/twoslash.css';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
-import { Analytics } from "@vercel/analytics/react"
+import { RootProvider } from "fumadocs-ui/provider";
+import "fumadocs-ui/style.css";
+import "fumadocs-ui/css/dusk.css";
+import "fumadocs-twoslash/twoslash.css";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
-      <meta name="description" content="Forget about giant Redstone logic gates. All logic gates in one block!" />
-      <meta name="keywords" content="redstone, minecraft plugin, spigot, bukkit, mechanics, logic gates minecraft" />
+      <meta
+        name="description"
+        content="Forget about giant Redstone logic gates. All logic gates in one block!"
+      />
+      <meta
+        name="keywords"
+        content="redstone, minecraft plugin, spigot, bukkit, mechanics, logic gates minecraft"
+      />
       <meta name="author" content="Piotr Bednarski" />
       <link rel="icon" href="/favicon.ico" />
       <body
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          minHeight: '100vh',
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
         }}
       >
         <RootProvider>
